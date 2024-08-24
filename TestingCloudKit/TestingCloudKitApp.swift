46480adead8e847b946f3b98f15a16abe91a7778
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2024-08-24 13:09:08 -0300

    first commit

---
+++ TestingCloudKit/TestingCloudKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingCloudKitApp.swift
+//  TestingCloudKit
+//
+//  Created by thaxz on 24/08/24.
+//
+
+import SwiftUI
+
+@main
+struct TestingCloudKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
